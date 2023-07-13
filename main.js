090cbddaebf38f825cc2694d91465aeba82e6501
paulweb1979 <38223037+paulweb1979@users.noreply.github.com>
2023-07-13 15:16:19 +0100

    1st version

---
+++ main.js
@@ -0,0 +1,12 @@
+// main.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Find the button element
+    var experienceBtn = document.getElementById('experienceBtn');
+  
+    // Add click event listener to the button
+    experienceBtn.addEventListener('click', function () {
+      // Navigate to webxr.html
+      window.location.href = 'webxr.html';
+    });
+  });
+  
